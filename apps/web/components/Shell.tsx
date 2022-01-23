91033949a47daa6fa3e4872c28e6b7c81213d9e9
Philip Niedertscheider <dev@philpri.me>
2022-01-23 16:02:52 +0100

    Removed license banner

--- apps/web/components/Shell.tsx
+++ apps/web/components/Shell.tsx
@@ -407,7 +407,6 @@ export default function Shell(props: {
               {/* add padding to content for mobile navigation*/}
               <div className="block pt-12 md:hidden" />
             </div>
-            <LicenseBanner />
           </main>
         </div>
       </div>
